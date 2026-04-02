64c0349cfeac099e84c7fc0c9f6c203c54aa822c
dinis <dinis.abuzarov@mail.ru>
2026-04-02 11:38:04 +0400

    pw-dev Аннотация конфигурации

---
+++ message-sender/src/main/java/pro/entera/message_sender/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package pro.entera.message_sender.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AppProperties.class)
+public class ApplicationConfig {}
