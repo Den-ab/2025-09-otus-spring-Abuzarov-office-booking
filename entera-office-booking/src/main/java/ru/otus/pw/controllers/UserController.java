830d93a5649b82313c65f723e77892b2855e049c
dinis <dinis.abuzarov@mail.ru>
2026-04-06 00:13:13 +0400

    pw-dev Эндпоинт логина

---
+++ entera-office-booking/src/main/java/ru/otus/pw/controllers/UserController.java
@@ -0,0 +1,60 @@
+package ru.otus.pw.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.pw.controllers.request_dtos.LoginDTO;
+import ru.otus.pw.controllers.response_dtos.AuthResponse;
+import ru.otus.pw.services.EnteraUserDetailsService;
+import ru.otus.pw.services.JwtService;
+
+/**
+ * Контроллер для работы с пользователями.
+ */
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+    //region Fields
+
+    /**
+     * Сервис для управления аутентификацией.
+     */
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     * Сервис для работы с данными пользователя.
+     */
+    private final EnteraUserDetailsService enteraUserDetailsService;
+
+    /**
+     * Сервис для работы с JWT.
+     */
+    private final JwtService jwtService;
+
+    //endregion
+    //region Public
+
+    /**
+     * Возвращает список пространств.
+     *
+     * @return Список пространств.
+     */
+    @PostMapping(value = "/auth/login")
+    public AuthResponse login(@RequestBody LoginDTO loginDTO) {
+
+        this.authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password())
+        );
+
+        UserDetails userDetails = this.enteraUserDetailsService.loadUserByUsername(loginDTO.email());
+        String token = this.jwtService.generateToken(userDetails);
+
+        return new AuthResponse(token);
+    }
+
+    //endregion
+}
